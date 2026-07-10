3a586acaa630f5493e24a3cdb86d5f48d576a77b
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-07-10 06:04:18 +0400

    Added retrieving of games using Steam ID

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import os
+import requests
+from dotenv import load_dotenv
 from flask import Flask, jsonify
+
+
+load_dotenv()
+API_KEY = os.getenv("STEAM_API_KEY")
+base_url = "https://api.steampowered.com"
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,8 +15,25 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Hii its flask api"})
 
+def get_user_games(steam_id):
+    url = f"{base_url}/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&steamid={steam_id}&format=json&include_appinfo=1"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("User game library retrieved")
+        user_games_data = response.json()
+        return user_games_data
+    else:
+        print("Failed to retrieve user game library")
+
+Steam_id = input("Enter your Steam ID: ")
+User_games_data = get_user_games(Steam_id)
+response_data = User_games_data.get("response", {})
+games_list = response_data.get("games", [])
+
+for game in games_list:
+    print(game["name"])
+print(f"Total games: {len(games_list)}")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-def get_user_games():
-    pass
