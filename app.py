ebe6b765bb3cc191181b419093e15416dc0cf321
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-07-11 21:03:40 +0400

    Username is now retrieved using steam id and shown with the games list

--- app.py
+++ app.py
@@ -20,24 +20,36 @@ def home():
 def submit():
     Steam_id = request.args.get("SteamID")
     Total_games = get_user_games(Steam_id)
-    return Total_games
+    Username = get_username(Steam_id)
+    return f"{Username} {Total_games}"
     #print(get_user_games(Steam_id))
 
 
 def get_user_games(steam_id):
     url = f"{base_url}/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&steamid={steam_id}&format=json&include_appinfo=1"
     response = requests.get(url)
-    Games_list = []
+    Games_name_list = []
     if response.status_code == 200:
         print("User game library retrieved")
         user_games_data = response.json()
         response_data = user_games_data.get("response", {})
-        response_games_list = response_data.get("games", [])
-        for game in response_games_list:
-            Games_list.append(game["name"])
-        return Games_list
+        games_list = response_data.get("games", [])
+        for game in games_list:
+            Games_name_list.append(game["name"])
+        return Games_name_list
     else:
         print("Failed to retrieve user game library")
+
+def get_username(steam_id):
+    url = f"{base_url}/ISteamUser/GetPlayerSummaries/v0002/?key={API_KEY}&steamids={steam_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("User name retrieved")
+        user_name_data = response.json()
+        response_data = user_name_data.get("response", {})
+        players_list = response_data.get("players", [])
+        username = players_list[0].get("personaname")
+        return f"User: {username}"
 
 #Steam_id = input("Enter your Steam ID: ")
 #User_games_data = get_user_games(Steam_id)
